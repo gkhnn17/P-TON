a7c45d218b56b3373f41d566324308ea91acd027
gkhnn17 <63924263+gkhnn17@users.noreply.github.com>
2023-11-26 14:08:21 +0300

    Delete CAS directory

--- CAS/cas2/main.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from functions import Interface
-
-app = QApplication([])
-window = Interface()
-window.show()
-app.exec_()    
--- CAS/cas4/odev4/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-contours_draw
